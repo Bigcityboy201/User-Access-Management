0408df7aea0f5c33fca865ba948ab452192d9e49
truonggg <ngoquangtruong2012004@gmail.com>
2025-12-24 16:23:45 +0700

    feat:add commentss v1

--- auth-service/src/main/java/com/r2s/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/r2s/auth/controller/AuthController.java
@@ -21,7 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping(path = "/auth")
 public class AuthController {
 
-	// 1234
+	// 123
 	private final UserService userService;
 	private final AuthenticationManager authenticationManager;
 	private final JwtUtils jwtUtils;
